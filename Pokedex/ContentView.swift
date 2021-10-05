ec8bae6a948bd070f0e9ad1b3bb9cb2a04b78298
Stanro22 <stanroozendaal@hotmail.com>
2021-10-05 14:09:22 +0200

    exercise 3

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pokemons: [Pokemon] = Pokemon.testList
+    //let pokemons: [Pokemon] = Pokemon.testList
 
-     var body: some View {
-         List(pokemons) { pokemon in
-             PokemonCell(pokemon: pokemon)
-         }
-     }
+     //var body: some View {
+         //List(pokemons) { pokemon in
+           //  PokemonCell(pokemon: pokemon)
+         //}
+     //}
+    
+    var columns = [
+        GridItem(.adaptive(minimum: 250), spacing: 0, alignment: .topLeading)
+    ]
+
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 0) {
+                ForEach(0 ..< 20) { item in
+                    PokemonCell(pokemon: .testPokemon)
+                }
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()	
+        ContentView()
     }
 }
